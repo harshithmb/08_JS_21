10357b8aa30bcf7919d087548cf3a073dff5a162
edyion <info@edyion.com>
2021-08-16 07:34:29 +0530

    Products search

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ const clothing = document.querySelector("#clothing-content");
 const cart = document.getElementById("cart");
 const cartItems = JSON.parse(window.localStorage.getItem("cart")) || [];
 cart.textContent = cartItems.length;
+
 fetch(PRODUCTS_URL)
   .then((res) => res.json())
   .then((res) => {
-    res.slice(0, 4).map((item, id) => {
+    originalProducts = res;
+    originalProducts.slice(0, 4).map((item, id) => {
       const card = document.createElement("div");
       card.className = "card m-2";
       card.style.width = "18rem";
@@ -44,3 +46,86 @@ fetch(PRODUCTS_URL)
       clothing.append(card);
     });
   });
+
+const searchValue = document.querySelector("#search").value;
+
+let originalProducts = []; // 10 products
+
+products =
+  searchValue &&
+  originalProducts.filter((item) => item.name.includes(searchValue));
+if (products.length) {
+  products.map((item, id) => {
+    const card = document.createElement("div");
+    card.className = "card m-2";
+    card.style.width = "18rem";
+    //   card.addEventListener("click", () => {
+    //     window.location.href = `/product/${item.id}`;
+    //   });
+
+    const cardImg = document.createElement("img");
+    cardImg.className = "card-img-top";
+    cardImg.src = item.preview;
+    card.append(cardImg);
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = item.name;
+    cardBody.append(cardTitle);
+
+    const cardPrice = document.createElement("h5");
+    cardPrice.className = "card-title";
+    cardPrice.textContent = `Rs. ${item.price}`;
+    cardBody.append(cardPrice);
+
+    const cardBtn = document.createElement("a");
+    cardBtn.className = "btn btn-primary";
+    cardBtn.textContent = "More Details";
+    cardBtn.href = `/product.html?id=${id}`;
+    cardBody.append(cardBtn);
+
+    card.append(cardBody);
+
+    clothing.append(card);
+  });
+} else {
+  originalProducts.map((item, id) => {
+    const card = document.createElement("div");
+    card.className = "card m-2";
+    card.style.width = "18rem";
+    //   card.addEventListener("click", () => {
+    //     window.location.href = `/product/${item.id}`;
+    //   });
+
+    const cardImg = document.createElement("img");
+    cardImg.className = "card-img-top";
+    cardImg.src = item.preview;
+    card.append(cardImg);
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = item.name;
+    cardBody.append(cardTitle);
+
+    const cardPrice = document.createElement("h5");
+    cardPrice.className = "card-title";
+    cardPrice.textContent = `Rs. ${item.price}`;
+    cardBody.append(cardPrice);
+
+    const cardBtn = document.createElement("a");
+    cardBtn.className = "btn btn-primary";
+    cardBtn.textContent = "More Details";
+    cardBtn.href = `/product.html?id=${id}`;
+    cardBody.append(cardBtn);
+
+    card.append(cardBody);
+
+    clothing.append(card);
+  });
+}
